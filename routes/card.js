0160c780b0485e8f0e827815c4cfb1c543851f88
bestseob93 <bestseob93@gmail.com>
2016-07-07 02:22:39 +0900

    160707_hwan_hi

--- routes/card.js
+++ routes/card.js
@@ -146,11 +146,21 @@
     var card_id = req.body.card_id;
       connection.query('update card set memo = ? where card_id = ?;',
           //?로 표현하지 않고, ? 대신 어떤 값을 넣으면 그자리에 계속 그 값만 들어가게된다.
-          [update_memo, card_id]);
-      res.writeHead(302, {
-          'Location': '/'
-      });
-      res.end();
+          [update_memo, card_id], fucntion(err, cursor) {
+            if(!err) {
+              if(cursor[0]) {
+                res.json({
+                  result : true, reason : "update done"
+                });
+              } else {
+                res.json({
+                  result : false, reason : "no card"
+                });
+              }
+            } else {
+              res.sendStatus(503);
+            }
+          });
   });
 
 
