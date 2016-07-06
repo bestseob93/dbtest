c5878ab7e8aa20191a64456449f2a135191058fa
bestseob93 <bestseob93@gmail.com>
2016-07-07 02:39:12 +0900

    160707_hwan_0707

--- routes/card.js
+++ routes/card.js
@@ -146,7 +146,7 @@
     var card_id = req.body.card_id;
       connection.query('update card set memo = ? where card_id = ?;',
           //?로 표현하지 않고, ? 대신 어떤 값을 넣으면 그자리에 계속 그 값만 들어가게된다.
-          [update_memo, card_id], fucntion(err, cursor) {
+          [update_memo, card_id], function(err, cursor) {
             if(!err) {
               if(cursor[0]) {
                 res.json({
@@ -163,13 +163,19 @@
           });
   });
 
-
   router.get('/delete/:card_id', function(req, res) {
-      connection.query('delete from card where card_id=?;', [req.params.card_id]);
-      res.writeHead(302, {
-          'Location': '/'
+      connection.query('delete from card where card_id=?;', [req.params.card_id], function(error, cursor) {
+        if(!error) {
+          res.json({
+            result : true, reason : "카드 삭제 성공"
+          });
+        } else {
+          res.json({
+            result : false, reason : "카드 삭제 실퍠"
+          });
+        }
       });
-      res.end();
+
   });
 
   module.exports = router;
