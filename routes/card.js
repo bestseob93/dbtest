8746c72db7431e00cecbac47aefe6ff80ce02502
bestseob93 <bestseob93@gmail.com>
2016-07-06 21:23:23 +0900

    160706_hwan_card_update

--- routes/card.js
+++ routes/card.js
@@ -137,10 +137,12 @@
       });
   });
 
-  router.post('/update/:card_id', function(req, res) {
-      connection.query('update card set memo = ?;',
+  router.get('/update/:card_id', function(req, res) {
+    var update_memo = req.params.memo;
+    var card_id = req.params.card_id;
+      connection.query('update card set memo = ? where card_id = ?;',
           //?로 표현하지 않고, ? 대신 어떤 값을 넣으면 그자리에 계속 그 값만 들어가게된다.
-          [req.body.memo, req.params.card_id]);
+          [update_memo, card_id]);
       res.writeHead(302, {
           'Location': '/'
       });
