5801616ac330d790828c570a6367295df9ce3528
bestseob93 <bestseob93@gmail.com>
2016-07-07 02:04:35 +0900

    160707_hwan_hihihi

--- routes/card.js
+++ routes/card.js
@@ -141,9 +141,9 @@
       });
   });
 
-  router.get('/update/:card_id', function(req, res) {
-    var update_memo = req.params.memo;
-    var card_id = req.params.card_id;
+  router.post('/update', function(req, res) {
+    var update_memo = req.body.memo;
+    var card_id = req.body.card_id;
       connection.query('update card set memo = ? where card_id = ?;',
           //?로 표현하지 않고, ? 대신 어떤 값을 넣으면 그자리에 계속 그 값만 들어가게된다.
           [update_memo, card_id]);
