a195e3d9fb7fd3e75238566a6756e744a132e3b0
bestseob93 <bestseob93@gmail.com>
2016-07-07 22:39:50 +0900

    160707_realfinalsaaaaa

--- routes/auth.js
+++ routes/auth.js
@@ -11,22 +11,14 @@ var router = express.Router();
 var hasher = bkfd2Password();
 
 var options = {
-  'host' : 'appjam-ping.cfsveedruyrb.ap-northeast-2.rds.amazonaws.com',
-  'port' : '3306',
-  'user' : 'ping',
-  'password' : 'd85z85755',
-  'database' : 'pingdb'
+
 };
 
 var sessionstore = new mysqlstore(options);
 
 
 var connection = mysql.createConnection({
-  'host' : 'appjam-ping.cfsveedruyrb.ap-northeast-2.rds.amazonaws.com',
-  'port' : '3306',
-  'user' : 'ping',
-  'password' : 'd85z85755',
-  'database' : 'pingdb'
+
 });
 
 /* login */
